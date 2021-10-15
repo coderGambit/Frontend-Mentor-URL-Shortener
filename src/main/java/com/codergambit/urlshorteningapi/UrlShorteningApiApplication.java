edb6cb90857680ead2f98fb8778b0d9f495512fd
Rondell <rondellkeionborel@gmail.com>
2021-10-14 20:37:46 -0400

    first commit

---
+++ src/main/java/com/codergambit/urlshorteningapi/UrlShorteningApiApplication.java
@@ -0,0 +1,16 @@
+package com.codergambit.urlshorteningapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UrlShorteningApiApplication{
+
+	public static void main(String[] args){
+
+		SpringApplication.run(UrlShorteningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codergambit/urlshorteningapi/UrlShorteningApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codergambit.urlshorteningapi;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class UrlShorteningApiApplicationTests{
+
+	@Test
+	void contextLoads(){
+
+	}
+
+}
